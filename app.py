acee1eb01977aa87dce9c1ba7b3e1b327f6a735d
jayavibhavnk <97757366+jayavibhavnk@users.noreply.github.com>
2024-05-25 19:11:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,104 @@ import matplotlib.pyplot as plt
 from GraphRetrieval import GraphRAG, ImageGraphRAG
 from PIL import Image
 
+import os
+import heapq
+import pickle
+import networkx as nx
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors import NearestNeighbors
+from queue import PriorityQueue
+
+class GraphRAG():
+    def __init__(self):
+        self.graph = None
+        self.documents = None
+        self.embeddings = None
+        self.embedding_model = "all-MiniLM-L6-v2"
+        self.retrieval_model = "a_star"
+
+    def constructGraph(self, text, similarity_threshold=0, chunk_size=1250, chunk_overlap=100, metadata=True):
+        text_splitter = CharacterTextSplitter(
+            separator="\n\n",
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            length_function=len,
+            is_separator_regex=False,
+        )
+
+        pre_documents = text_splitter.create_documents([text])
+        documents = [GraphDocument(doc.page_content, doc.metadata) for doc in pre_documents]
+
+        model = SentenceTransformer(self.embedding_model)
+        embeddings = model.encode([doc.page_content for doc in documents])
+        graph = nx.Graph()
+
+        for i in range(len(documents)):
+            for j in range(i, len(documents)):
+                similarity = cosine_similarity([embeddings[i]], [embeddings[j]])
+                if similarity[0][0] > similarity_threshold:
+                    graph.add_edge(i, j, weight=similarity[0][0])
+
+        self.graph = graph
+        self.documents = documents
+        self.embeddings = embeddings
+
+        return graph, documents, embeddings
+
+    def create_graph_from_file(self, file, similarity_threshold=0):
+        with open(file, 'r') as file:
+            text_data = file.read()
+        self.graph, self.documents, self.embeddings = self.constructGraph(text_data, similarity_threshold=similarity_threshold)
+        print("Graph created Successfully!")
+
+    def create_graphs_from_directory(self, directory_path, similarity_threshold=0):
+        file_list = []
+        overall_text = ""
+        for file_name in os.listdir(directory_path):
+            if file_name.endswith(".txt"):
+                file_path = os.path.join(directory_path, file_name)
+                temp_text = open(file_path, 'r').read()
+                overall_text = overall_text + "\n" + temp_text
+                file_list.append((temp_text, file_name))
+
+        self.graph, self.documents, self.embeddings = self.constructGraph(overall_text, similarity_threshold=similarity_threshold)
+        print("Graph created Successfully!")
+
+        return file_list
+
+    def create_graph_from_text(self, text, similarity_threshold=0):
+        self.graph, self.documents, self.embeddings = self.constructGraph(text, similarity_threshold=similarity_threshold)
+        print("Graph created Successfully!")
+
+    def compute_similarity(self, current_node, graph, documents, query_embedding):
+        similar_nodes = []
+        for neighbor in graph.neighbors(current_node):
+            neighbor_embedding = self.embeddings[neighbor]
+            neighbor_similarity = cosine_similarity([query_embedding], [neighbor_embedding])[0][0]
+            similar_nodes.append((neighbor, neighbor_similarity))
+        return similar_nodes
+
+    def a_star_search_parallel(self, graph, documents, embeddings, query_text, k=5):
+        model = SentenceTransformer(self.embedding_model)
+        query_embedding = model.encode([query_text])[0]
+
+        pq = [(0, None, 0)]
+        visited = set()
+        similar_nodes = []
+
+        while pq and len(similar_nodes) < k:
+            _, current_node, similarity_so_far = heapq.heappop(pq)
+
+            if current_node is not None:
+                similar_nodes.append((current_node, similarity_so_far))
+
+            results = []
+            for neighbor in (graph.neighbors(current_node) if current_node is not None else range(len(documents)-1)):
+                results.extend(self.compute_similarity(neighbor, graph, documents, query_embedding))
+
+
+
 st.title("Graph Retrieval System")
 
 # Initialize session state
